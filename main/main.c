7216be908fb166e2d3cfe3ce198f04d6cd70476d
TTThang <42745587+TTThang@users.noreply.github.com>
2024-10-08 10:34:47 +0700

    init project

---
+++ main/main.c
@@ -0,0 +1,248 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <ina3221.h>
+#include <string.h>
+
+#include "freertos/projdefs.h"
+#include "mqtt.h"
+
+const char *TAG = "POWER-MONITOR";
+
+#define I2C_PORT I2C_NUM_0
+#define WARNING_CHANNEL 1
+#define WARNING_CURRENT (40.0)
+
+//#define STRUCT_SETTING 0
+#if defined EXAMPLE_MEASURING_MODE_TRIGGER
+#define MODE false  // true : continuous  measurements // false : trigger measurements
+#else
+#define MODE true
+#endif
+
+#define VOLTAGE_MAX 4.2
+#define VOLTAGE_MIN 3.0
+
+typedef struct {
+    float bus_voltage;
+    float current;
+    float power;
+    double total_mAh;
+    double total_mWh;
+    float battery_remain;
+} INA_Data_t;
+
+
+typedef struct {
+    INA_Data_t channel_1;
+    INA_Data_t channel_2;
+    INA_Data_t channel_3;
+    char timestamp[64];
+} INA_Data_Snapshot_t;
+
+INA_Data_Snapshot_t ina_data;
+
+char *power_data;
+int data_length;
+
+extern esp_mqtt_client_handle_t mqtt_client;
+
+float battery_capacitor_remain_cal(float batt_vol) {
+    if (batt_vol >= VOLTAGE_MAX) {
+        return 100.0;
+    } else if (batt_vol <= VOLTAGE_MIN) {
+        return 0.0;
+    } else {
+        return (batt_vol - VOLTAGE_MIN) / (VOLTAGE_MAX - VOLTAGE_MIN) * 100.0;
+    }
+}
+
+void task_push_data_to_mqtt(void* arg){
+	while(1){
+		esp_mqtt_client_publish(mqtt_client, "smart-farm/hub/sensor/power-monitor", power_data, data_length, 0, 1);
+		vTaskDelay(pdMS_TO_TICKS(5000));
+	}
+}
+
+void update_timestamp(INA_Data_Snapshot_t* snapshot) {
+    time_t now;
+    time(&now);
+
+    struct tm *timeinfo = gmtime(&now);
+
+    timeinfo->tm_hour += 7;
+
+    if (timeinfo->tm_hour >= 24) {
+        timeinfo->tm_hour -= 24;
+        timeinfo->tm_mday += 1;
+        mktime(timeinfo);
+    }
+
+    strftime(snapshot->timestamp, sizeof(snapshot->timestamp), "%Y-%m-%dT%H:%M:%SZ", timeinfo);
+}
+
+
+void update_ina_data(ina3221_t* dev,INA_Data_Snapshot_t* snapshot) {
+	float bus_voltage;
+    float shunt_voltage;
+    float shunt_current;
+    
+	ina3221_get_bus_voltage(dev, 0, &bus_voltage);
+    ina3221_get_shunt_value(dev, 0, &shunt_voltage, &shunt_current);
+    
+    ina_data.channel_1.bus_voltage = bus_voltage + (shunt_current)*0.1;
+    ina_data.channel_1.current = shunt_current;
+    ina_data.channel_1.total_mAh += (shunt_current/36000);
+    ina_data.channel_1.power = bus_voltage*shunt_current;
+    ina_data.channel_1.total_mWh += (ina_data.channel_1.power/36000);
+    
+    ina_data.channel_1.battery_remain = battery_capacitor_remain_cal(bus_voltage);
+
+	ina3221_get_bus_voltage(dev, 1, &bus_voltage);
+    ina3221_get_shunt_value(dev, 1, &shunt_voltage, &shunt_current);
+    
+    ina_data.channel_2.bus_voltage = bus_voltage + (shunt_current)*0.1;
+    ina_data.channel_2.current = shunt_current;
+    ina_data.channel_2.total_mAh += (shunt_current/36000);
+    ina_data.channel_2.power = bus_voltage*shunt_current;
+    ina_data.channel_2.total_mWh += (ina_data.channel_2.power/36000);
+
+    ina3221_get_bus_voltage(dev, 2, &bus_voltage);
+    ina3221_get_shunt_value(dev, 2, &shunt_voltage, &shunt_current);
+    
+    ina_data.channel_3.bus_voltage = bus_voltage + (shunt_current)*0.1;
+    ina_data.channel_3.current = shunt_current;
+    ina_data.channel_3.total_mAh += (shunt_current/36000);
+    ina_data.channel_3.power = bus_voltage*shunt_current;
+    ina_data.channel_3.total_mWh += (ina_data.channel_3.power/36000);
+
+    update_timestamp(snapshot);
+}
+
+void task_update_data(void *arg) {
+	while(1){
+		if(power_data != NULL){
+			free(power_data);
+		}
+	    data_length = asprintf(&power_data,
+		    "{\n"
+		    "\t\"channels\": [\n"
+		    "\t\t{\"channel\": 1, \"bus_voltage\": %.2f V, \"current\": %.2f mA, \"power\": %.2f mW, \"capacitor\": %.5f mAh, \"consumption\": %.5f mWh, \"battery\": %.2f %%},\n"
+		    "\t\t{\"channel\": 2, \"bus_voltage\": %.2f V, \"current\": %.2f mA, \"power\": %.2f mW, \"capacitor\": %.5f mAh, \"consumption\": %.5f mWh},\n"
+		    "\t\t{\"channel\": 3, \"bus_voltage\": %.2f V, \"current\": %.2f mA, \"power\": %.2f mW, \"capacitor\": %.5f mAh, \"consumption\": %.5f mWh}\n"
+		    "\t],\n"
+		    "\t\"timestamp\": \"%s\"\n"
+		    "}",
+		    ina_data.channel_1.bus_voltage, ina_data.channel_1.current, ina_data.channel_1.power,
+		    ina_data.channel_1.total_mAh, ina_data.channel_1.total_mWh, ina_data.channel_1.battery_remain,
+		    ina_data.channel_2.bus_voltage, ina_data.channel_2.current, ina_data.channel_2.power,
+		    ina_data.channel_2.total_mAh, ina_data.channel_2.total_mWh,
+		    ina_data.channel_3.bus_voltage, ina_data.channel_3.current, ina_data.channel_3.power,
+		    ina_data.channel_3.total_mAh, ina_data.channel_3.total_mWh,
+		    ina_data.timestamp);
+
+	
+	    ESP_LOGI("INA_DATA_JSON", "Updated JSON:\n%s", power_data);
+	    vTaskDelay(pdMS_TO_TICKS(200));
+	}
+}
+
+void task(void *pvParameters)
+{
+    ina3221_t dev = {
+            .shunt = {
+                100,
+                100,
+                100
+            },
+            .config.config_register = INA3221_DEFAULT_CONFIG,
+            .mask.mask_register = INA3221_DEFAULT_MASK
+    };
+    memset(&dev.i2c_dev, 0, sizeof(i2c_dev_t));
+
+    ESP_ERROR_CHECK(i2cdev_init());
+    ESP_ERROR_CHECK(ina3221_init_desc(&dev, 0x40, I2C_PORT, 48, 47));
+
+#ifndef STRUCT_SETTING
+    ESP_ERROR_CHECK(ina3221_set_options(&dev, MODE, true, true)); 
+    ESP_ERROR_CHECK(ina3221_enable_channel(&dev, true, true, true));
+    ESP_ERROR_CHECK(ina3221_set_average(&dev, INA3221_AVG_1024));
+    ESP_ERROR_CHECK(ina3221_set_bus_conversion_time(&dev, INA3221_CT_8244));
+    ESP_ERROR_CHECK(ina3221_set_shunt_conversion_time(&dev, INA3221_CT_8244));
+#else
+    dev.config.mode = MODE;
+    dev.config.esht = true;
+    dev.config.ebus = true;
+    dev.config.ch1 = true;
+    dev.config.ch2 = true;
+    dev.config.ch3 = true;
+    dev.config.avg = INA3221_AVG_1024;
+    dev.config.vbus = INA3221_CT_8244;
+    dev.config.vsht = INA3221_CT_8244;
+    ESP_ERROR_CHECK(ina3221_sync(&dev));
+#endif
+
+    ESP_ERROR_CHECK(ina3221_set_warning_alert(&dev, WARNING_CHANNEL - 1, WARNING_CURRENT)); // Set overcurrent security flag
+
+    /*uint32_t measure_number = 0;
+    bool warning = false;
+    float bus_voltage;
+    float shunt_voltage;
+    float shunt_current;*/
+
+    while (1)
+    {
+       //  measure_number++;
+
+/*#if CONFIG_EXAMPLE_MEASURING_MODE_TRIGGER
+        ESP_ERROR_CHECK(ina3221_trigger(&dev)); // Start a measure
+        printf("trig done, wait: ");
+        do
+        {
+            printf("X");
+
+            ESP_ERROR_CHECK(ina3221_get_status(&dev)); // get mask
+
+            if (dev.mask.wf & (1 << (3 - WARNING_CHANNEL)))
+                warning = true;
+
+            vTaskDelay(pdMS_TO_TICKS(20));
+
+        } while (!(dev.mask.cvrf)); // check if measure done
+#else
+        ESP_ERROR_CHECK(ina3221_get_status(&dev)); // get mask
+
+        if (dev.mask.wf & (1 << (3 - WARNING_CHANNEL)))
+            warning = true;
+#endif*/
+        /*for (uint8_t i = 0; i < INA3221_BUS_NUMBER; i++)
+        {
+            // Get voltage in volts
+            ESP_ERROR_CHECK(ina3221_get_bus_voltage(&dev, i, &bus_voltage));
+            // Get voltage in millivolts and current in milliamperes
+            ESP_ERROR_CHECK(ina3221_get_shunt_value(&dev, i, &shunt_voltage, &shunt_current));
+
+            printf("\nC%u:Measure number %" PRIu32 "\n", i + 1, measure_number);
+            if (warning && (i + 1) == WARNING_CHANNEL)
+                printf("C%u:Warning Current > %.2f mA !!\n", i + 1, WARNING_CURRENT);
+            printf("C%u:Bus voltage: %.02f V\n", i + 1, bus_voltage);
+            printf("C%u:Shunt voltage: %.02f mV\n", i + 1, shunt_voltage);
+            printf("C%u:Shunt current: %.02f mA\n\n", i + 1, shunt_current);
+        }*/
+        
+        update_ina_data(&dev, &ina_data);
+       // warning = false ;
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+}
+
+void app_main()
+{
+	memset(&ina_data, 0, sizeof(INA_Data_Snapshot_t));
+	init_mqtt();
+    xTaskCreate(task, "ina3221_test", configMINIMAL_STACK_SIZE * 8, NULL, 20, NULL);
+    xTaskCreate(task_push_data_to_mqtt, "task_push_data_to_mqtt", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL);
+    xTaskCreate(task_update_data, "task_update_data", configMINIMAL_STACK_SIZE * 8, NULL, 10, NULL);
+}
---
+++ main/mqtt.h
@@ -0,0 +1,198 @@
+/*
+ * mqtt.h
+ *
+ *  Created on: 26 thg 9, 2024
+ *      Author: thang
+ */
+
+#ifndef MAIN_MQTT_H_
+#define MAIN_MQTT_H_
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "WiFiConnection.h"
+
+#include "esp_sntp.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char *TAG;
+
+void time_sync_notification_cb(struct timeval *tv) {
+    ESP_LOGI(TAG, "Time synchronized successfully");
+}
+
+void initialize_sntp(void) {
+    ESP_LOGI(TAG, "Initializing SNTP");
+
+    // Thiết lập SNTP và đăng ký callback khi thời gian được đồng bộ
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, "pool.ntp.org");  // Sử dụng server NTP phổ biến
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+
+    // Bắt đầu đồng bộ NTP
+    esp_sntp_init();
+}
+
+void obtain_time(void) {
+    initialize_sntp();
+
+    // Đợi thời gian được đồng bộ
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+    
+    while (timeinfo.tm_year < (2020 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);  // Chờ 2 giây
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+
+    if (retry == retry_count) {
+        ESP_LOGI(TAG, "Failed to sync time with NTP server");
+    }
+}
+
+esp_mqtt_client_config_t mqtt_cfg = {
+    .broker.address ={
+		.uri = "mqtt://192.168.1.22:1883",
+	},
+    .credentials = {
+		.username = "tutru20139",
+		.authentication.password = "20139"
+	},
+};
+
+esp_mqtt_client_handle_t mqtt_client;
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+    mqtt_client = client;
+}
+
+
+
+void init_mqtt(void)
+{
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+    esp_log_level_set("mqtt_example", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("transport", ESP_LOG_VERBOSE);
+    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    WiFi_STA_Connect("Alpha Bits", "second.brains", WIFI_AUTH_WPA2_PSK);
+	
+	obtain_time();
+	
+    mqtt_app_start();
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MAIN_MQTT_H_ */
